6c48dcec7e2dd119baf8246715eed1b4bf57b758
Diego Ramos dos Santos <diegorsantos09@outlook.com>
2026-09-25 00:50:20 -0300

    Stop tracking generated files

---
+++ ExerciciosCsharp/Exercicios/ExercicioGenerics.cs
@@ -0,0 +1,46 @@
+﻿namespace ExerciciosCsharp.Exercicios;
+
+public class ExercicioGenerics
+{
+    
+    private static readonly
+        ExercicioLinq _products = new();
+    public class Repository<T>
+    { 
+        private readonly List<T> _items = new();
+
+        public void add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void remove(T item)
+        {
+            _items.Remove(item);
+        }
+
+        public List<T> list()
+        {
+            return _items;
+        }
+
+        public int count()
+        {
+            return _items.Count;
+        }
+    }
+
+    public void Executar()
+    {
+        var productRepository = new Repository<Product>();
+        var repositoryString = new Repository<string>();
+
+        foreach (var produto in _products.Products.Take(3))
+        {
+            productRepository.add(produto);
+        }
+        
+        repositoryString.add("Olá");
+        repositoryString.add("Até mais");
+    }
+}
